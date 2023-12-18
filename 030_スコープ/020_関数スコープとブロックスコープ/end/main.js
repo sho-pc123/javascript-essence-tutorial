c61787e4968f0d4d17b075d8b2de3579ec541817
sho-pc123 <syouteni123@gmail.com>
2023-12-19 08:37:03 +0900

    ぶろっ くすこーぷについて

--- 030_スコープ/020_関数スコープとブロックスコープ/end/main.js
+++ 030_スコープ/020_関数スコープとブロックスコープ/end/main.js
@@ -8,10 +8,10 @@ a();
 if(true) {
     let c = 1;
     
-    let d = function() {
+    const d = function() {
         console.log('d is called');
     }
 }
-// 関数宣言では、ブロックスコープは無視して実行できる（変数に代入すると）
+// 関数宣言では、ブロックスコープは無視して実行できる（変数に代入すると呼び出すことはできなくなる）
 d();
 console.log(c);
